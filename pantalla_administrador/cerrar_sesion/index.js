fe6b380efcefabad4cf046db37bf3017bbe101a1
ddimatee <brayandimate2023@gmail.com>
2025-04-23 06:50:06 -0500

    se cambia inicio de sesion y pantalla de recuperar contraseña (Admin)

--- pantalla_administrador/cerrar_sesion/index.js
+++
@@ -1,9 +0,0 @@
-function restablecercontraseña() {
-    // Ocultar el formulario de inicio de sesión
-    document.getElementById("loginForm").style.display = "none";
-     // Ocultar el botón de "¿Olvidaste tu contraseña?"
-     document.getElementById("recuperarcontraseña").style.display = "none";
-    // Mostrar el contenedor de recuperación de contraseña
-    document.getElementById("contenedorrecuperarcontraseña").style.display = "block";
-    
-}
